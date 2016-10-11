b260ed1cebf474d5914a215bec3f17605a91523c
nikhiljindal <nikhiljindal@google.com>
2016-10-11 12:48:38 -0700

    Adding cascading deletion support to federated namespaces

--- test/e2e/framework/util.go
+++ test/e2e/framework/util.go
@@ -2203,6 +2203,7 @@ func DumpEventsInNamespace(eventsLister EventsLister, namespace string) {
 	events, err := eventsLister(v1.ListOptions{}, namespace)
 	Expect(err).NotTo(HaveOccurred())
 
+	By(fmt.Sprintf("Found %d events.", len(events.Items)))
 	// Sort events by their first timestamp
 	sortedEvents := events.Items
 	if len(sortedEvents) > 1 {
